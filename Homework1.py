c804821ee2c4b1dd39d53502ae440bc52577fd2f
burdav22 <burdav22@ccm-2610-c3.evergreen.edu>
2013-10-03 13:51:01 -0700

    Initial Commit
    Initial Commit

---
+++ Homework1.py
@@ -0,0 +1,59 @@
+# Name: David Burke
+# Evergreen Login: burdav22@evergreen.edu
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###           ###
+### Problem 1 ###
+###           ###
+
+print "IT LIVES!"               
+
+print "Problem 1 solution follows:"
+
+b = -5.86 
+-b + (math.sqrt((b**2)-(4*1*8.5408))) #x is equal to negative b plus...
+b/2 * 1 #All divided by 2a!
+print "The first root is:"
+print b
+
+
+b = -5.86
+-b - (math.sqrt((b**2)-(4*1*8.5408))) #x is equal to negative b 
+b/2
+print "The second root is:"
+print b
+
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
