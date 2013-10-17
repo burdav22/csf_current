ca08e99597de84fa99c275a5ba5db25cbd606544
burdav22 <burdav22@ccm-2610-c3.evergreen.edu>
2013-10-17 14:37:33 -0700

    dasf
    adsf

---
+++ Lab3.py
@@ -0,0 +1,15 @@
+a = 1
+b = 1
+count = 0
+for i in range(1,11):
+    print a
+    n = a + b
+    a = b
+    b = n
+    count = count + 1
+print ("")
+
+print 'the',count,'th fibonacci number'
+
+
+elif 
