716c96274f0c0ab36045a9baf712ccb4ae9c9262
burdav22 <burdav22@users.noreply.github.com>
2013-10-24 14:21:07 -0700

    Update Lab3.py
    finished

--- Lab3.py
+++ Lab3.py
@@ -1,15 +1,21 @@
 a = 1
 b = 1
-count = 0
+fibonacci = 0
+series = 0
 for i in range(1,11):
     print a
     n = a + b
     a = b
     b = n
-    count = count + 1
+    fibonacci = fibonacci + 1
 print ("")
 
-print 'the',count,'th fibonacci number'
+print 'the',fibonacci,'th fibonacci number'
 
 
-elif 
+if series == fibonacci:
+    print ('invalid string')
+    
+    
+
+
